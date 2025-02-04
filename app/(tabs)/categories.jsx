8f2996f602c8feb3bef194e900eb7496eaaf0633
buudangfelix <buudangg@gmail.com>
2025-02-04 14:04:32 +0930

    Changed Category Tab UI

--- app/(tabs)/categories.jsx
+++ app/(tabs)/categories.jsx
@@ -1,9 +1,9 @@
-// Updated Category.js
 import React, { useState } from 'react';
-import { View, FlatList, SafeAreaView, TouchableOpacity, Text } from 'react-native';
+import { View, FlatList, ScrollView, TouchableOpacity, Text } from 'react-native';
 import WeaponCard from '@/components/WeaponCard';
 import Header from '@/components/Header';
 import LoadingIndicator from '@/components/LoadingIndicator';
+
 
 const Category = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -11,7 +11,7 @@ const Category = () => {
   const [categoryProducts, setCategoryProducts] = useState([]);
 
   return (
-    <SafeAreaView className="flex-1 bg-primary">
+    <View className="bg-primary h-full">
       <LoadingIndicator isLoading={isSubmitting} />
       <Header
         selectedCategory={selectedCategory}
@@ -33,38 +33,57 @@ const Category = () => {
           className='px-4'
         />
       ) : (
-        <FlatList
+        <FlatList>
+          <View className="px-4">
+            <Text className="text-center text-4xl font-extrabold text-white tracking-wide">
+              Search for CS2 skins for your weapon of choice
+            </Text>
+            <Text className="text-center text-2xl text-gray-200 mt-3 leading-relaxed">
+              CS2 features a total of 53 weapons consisting of 10 pistols, 
+              11 rifles, 7 SMGs, 6 heavy weapons, and 20 knives (excluding the default CT and T knives). 
+              An assortment of over 1,000 <Text className="text-blue-300">CS2 skins</Text> are available 
+              to customise these weapons.
+            </Text>
+          </View>
+          <View className="flex-row flex-wrap justify-center mb-10">
+            {Object.keys(categoryProducts).map((item) => (
+              <TouchableOpacity
+                key={item}
+                onPress={() => setSelectedCategory(item)}
+                className="bg-blue-600 py-3 w-2/5 m-2.5 rounded-lg items-center justify-center"
+              >
+                <Text className="text-white text-2xl font-psemibold text-center">
+                  {item.toUpperCase()}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+          <FlatList
           data={Object.keys(categoryProducts)}
           keyExtractor={(item) => item}
           renderItem={({ item }) => (
             <View className="pt-4">
-              <View className="flex-row justify-between items-center px-4">
-                <Text className="text-gray-100 text-lg font-psemibold">
-                  {item.toUpperCase()}
+              <View>
+                <Text className="text-gray-100 text-4xl font-psemibold mb-2 border-t border-gray-400 pt-4 text-center">
+                  {item.charAt(0).toUpperCase() + item.slice(1)}
                 </Text>
-                <TouchableOpacity onPress={() => setSelectedCategory(item)}>
-                  <Text className="text-white text-sm font-psemibold underline">
-                    View More
-                  </Text>
-                </TouchableOpacity>
               </View>
               <FlatList
-                data={categoryProducts[item]?.slice(0, 2)}
+                className='px-4'
+                data={categoryProducts[item]?.slice(0)}
                 keyExtractor={(product) => product.$id}
                 renderItem={({ item: product }) => (
                   <WeaponCard
                     item={product}
-                    isSubmitting={isSubmitting}
-                    setIsSubmitting={setIsSubmitting}
                   />
                 )}
               />
             </View>
           )}
-          className='px-4'
-        />
+        /> 
+        </FlatList>
       )}
-    </SafeAreaView>
+    </View>
   );
 };
 
